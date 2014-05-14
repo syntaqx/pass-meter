0cbf0e86ac0ce0aea4b04baf814aa300e087c516
Chase Hutchins <syntaqx@gmail.com>
2014-05-14 11:08:41 -0600

    adding basic strength testing and test elaboration

--- src/pass-meter.js
+++ src/pass-meter.js
@@ -25,26 +25,60 @@
 }((typeof window !== 'undefined' ? window : this), function ($) {
 
   var defaultOptions = {
+    events:    'keyup',
     afterTest: null // callback function (if you want)
   }
 
   var PassMeter = function (options) {
+    // allows the afterTest to be specified as the options object
+    if (typeof options === 'function') {
+      options = {
+        afterTest: options
+      }
+    }
+
     this.options = $.extend({}, defaultOptions, options)
-    this.initialize()
   }
 
   PassMeter.prototype = {
 
     constructor: PassMeter,
 
-    initialize: function () {
+    // Regular expression tests
+    tests: {
+      uppercase: new RegExp('[A-Z]'),
+      lowercase: new RegExp('[a-z]'),
+      numbers:   new RegExp('[0-9]'),
+      symbols:   new RegExp('([!,%,&,@,#,$,^,*,?,_,~])')
     },
 
-    test: function (value) {
-      var score
+    getPercentage: function (a, b) {
+      return ((b / a) * 100)
+    },
 
-      // @TODO: Actually determine score from password complexity
-      score = (Math.floor(Math.random() * 100) + 1)
+    // Test the strength of a given value
+    // @TODO: This needs to be a decent algorithm. It's current pretty
+    // simplistic and could benefit greatly.
+    test: function (value) {
+      var uppercase  = 0,
+          lowercase  = 0,
+          numbers    = 0,
+          symbols    = 0,
+          total      = 0,
+          score      = 0
+
+      // They can't have a good password less than 8 characters.
+      if (value.length >= 8) {
+        if (value.match(this.tests.uppercase)) uppercase  = 1
+        if (value.match(this.tests.lowercase)) lowercase  = 1
+        if (value.match(this.tests.numbers))   numbers    = 1
+        if (value.match(this.tests.symbols))   symbols    = 1
+
+        total = uppercase + lowercase + numbers + symbols
+        score = (total / 4) * 100
+      } else {
+        score = 5
+      }
 
       // Run an afterTest callback if defined
       if (typeof this.options.afterTest === 'function') {
@@ -66,10 +100,12 @@
       if (!$el.data('pass-meter')) {
         var obj = new PassMeter(options)
 
-        $el.data('pass-meter', obj)
-        $el.on('keyup', function () {
+        // Bind to the events specified in the options
+        $el.on(obj.options.events, function () {
           obj.test(this.value)
         })
+
+        $el.data('pass-meter', obj)
       }
     })
   }
